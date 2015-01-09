83da6b4c69ba75f3fa85d298e579538e388cefef
Marcin Bonk <marcin.bonk@gowork.pl>
2015-01-09 13:26:04 +0100

    removed multiply kurwa next to kurwa

--- skrypt.js
+++ skrypt.js
@@ -1,42 +1,53 @@
 (function () {
-    var
-            alltexts = function (n) {
-                var result = [];
-                if (n) {
-                    n = n.firstChild;
-                    while (n !== null) {
-                        if (n.nodeType === 3) {
-                            result.push(n);
-                        } else {
-                            result = result.concat(alltexts(n));
-                        }
-                        n = n.nextSibling;
-                    }
-                }
-                return result;
-            },
-            forEach = function (a, callback, thisArg) {
-                if (typeof (callback) !== "function") {
-                    throw new TypeError(callback + " is not a function!");
-                }
-                var len = a.length;
-                for (var i = 0; i < len; i++) {
-                    callback.call(thisArg, a[i], i, a)
-                }
-            };
-
-    forEach(alltexts(document.body), function (element) {
-        if (Math.random() * 1 > 0.62) {
-            var t = element.textContent.split(' ');
-            forEach(t, function (val, k) {
-                if (val.length > 1 && k > 0 && Math.random() * 1 > 0.67) {
-                    t[k] = 'kurwa ' + val;
-                }
-            });
-            try {
-                element.textContent = t.join(' ');
-            } catch (e) {
-            }
+  var
+  alltexts = function (n) {
+    var result = [
+    ];
+    if (n) {
+      n = n.firstChild;
+      while (n !== null) {
+        if (n.nodeType === 3) {
+          result.push(n);
+        } else {
+          result = result.concat(alltexts(n));
         }
-    });
-})();
+        n = n.nextSibling;
+      }
+    }
+    return result;
+  },
+  forEach = function (a, callback, thisArg) {
+    if (typeof (callback) !== 'function') {
+      throw new TypeError(callback + ' is not a function!');
+    }
+    var len = a.length;
+    for (var i = 0; i < len; i++) {
+      callback.call(thisArg, a[i], i, a)
+    }
+  };
+  forEach(alltexts(document.body), function (element) {
+    if (Math.random() * 1 > 0.62) {
+      var t = element.textContent.split(' ');
+      forEach(t, function (val, k) {
+        if (val.length > 1 && k > 0 && Math.random() * 1 > 0.67) {
+          var mixed = [
+            'się',
+            'i',
+            'z',
+            'o',
+            'po',
+            'przed',
+          ];
+          if (typeof [k + 1] != 'undefined' && mixed.indexOf(t[k + 1]) == -1 && [t[k-1],t[k],t[k+1]].indexOf('kurwa') == -1) {
+            t[k] = 'kurwa ' + val;
+          }
+        }
+      });
+      try {
+        element.textContent = t.join(' ');
+      } catch (e) {
+      }
+    }
+  });
+}) ();
+
